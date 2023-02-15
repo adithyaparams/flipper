7b54a90c8edbcf256fb77754c34bf6985e1a8f79
Adithya Paramasivam <adithyaparams@berkeley.edu>
2023-02-15 00:17:35 +0000

    Get code running in nb

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping
 from cnn import CNN, CNNTokenizer
@@ -7,28 +8,37 @@ from pathlib import Path
 
 RESULTS_DIR = 'results/'
 
+def create_parser():
+    parser = argparse.ArgumentParser(description="train esm")
+    parser.add_argument("dataset", type=str)
+    parser.add_argument("split", type=str)
+    parser.add_argument("model", choices = ["ridge", "cnn", "esm1b", "esm1v", "esm_rand"], type = str)
+    parser.add_argument("max_epochs", type=int)
+    return parser
+
 if __name__ == "__main__":
     dataset = 'gb1'
     model = 'cnn'
-    split = 'two_vs_rest'
+    split = 'three_vs_rest'
     kernel_size = 5
     input_size = 1024
     dropout = 0.0
     batch_size = 256
+    
+    parser = create_parser()
+    args = parser.parse_args()
 
     results_dir = Path(RESULTS_DIR)
     results_dir.mkdir(exist_ok=True)
     model = CNN(kernel_size, input_size, dropout)
-    gb1 = DataModule('gb1', '{}.csv'.format(split), batch_size, CNNTokenizer())
+    gb1 = DataModule(args.dataset, '{}.csv'.format(args.split), batch_size, CNNTokenizer())
     # max_epochs for cnn is 100, esm is 500
-    trainer = Trainer(callbacks=[EarlyStopping(monitor='val_spearman', mode='max', patience=20)])
+    trainer = Trainer(callbacks=[EarlyStopping(monitor='val_spearman', mode='max', patience=20)], accelerator='gpu', devices=[0], max_epochs=args.max_epochs)
     # trainer = Trainer(callbacks=[EarlyStopping(monitor='val_loss', mode='min', patience=20)]) # ESM trainer
     trainer.fit(model, datamodule=gb1)
 
     val_dict = trainer.validate(datamodule=gb1)
     test_dict = trainer.test(datamodule=gb1)
 
-    print(val_dict, test_dict)
-
-    with open(results_dir / (dataset+'_results.csv'), 'a', newline='') as f:  
-        writer(f).writerow([dataset, model, split, kernel_size, input_size, dropout, batch_size, *val_dict[0].values(), *test_dict[0].values()])
+    with open(results_dir / (args.dataset+'_results.csv'), 'a', newline='') as f:  
+        writer(f).writerow([args.dataset, args.model, args.split, kernel_size, input_size, dropout, batch_size, *val_dict[0].values(), *test_dict[0].values()])
