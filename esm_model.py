d3a6ce916132e9ca0fe07407a16a92b9a1c03e59
Adithya Paramasivam <adithyaparams@berkeley.edu>
2023-03-23 09:24:16 +0000

    Add optimizers, no grad on embedding generation, test spearman, and clean up other bugs (flatten output/tgt, just initialize alphabet in tokenizer)

--- esm_model.py
+++ esm_model.py
@@ -2,8 +2,8 @@ from typing import Tuple
 import torch
 import pytorch_lightning as pl
 from torch.nn import functional as F
-from torchmetrics import MeanSquaredError
-from esm import pretrained
+from torchmetrics import MeanSquaredError, SpearmanCorrCoef
+from esm import pretrained, data
 from modules import Attention1d
 from data import Tokenizer
 
@@ -14,7 +14,7 @@ class ESM(pl.LightningModule):
     }
 
     """Outputs of the ESM model with the attention1d"""
-    def __init__(self, model_name, d_embedding, pooling="per_aa"): # [batch x sequence(751) x embedding (1280)] --> [batch x embedding] --> [batch x 1]
+    def __init__(self, model_name, d_embedding, pooling="per_aa", lr=0.001): # [batch x sequence(751) x embedding (1280)] --> [batch x embedding] --> [batch x 1]
         super(ESM, self).__init__()
         if model_name not in ESM.name_to_model.keys():
             raise ValueError("Invalid model name provided")
@@ -29,39 +29,61 @@ class ESM(pl.LightningModule):
         self.linear = torch.nn.Linear(d_embedding, d_embedding)
         self.relu = torch.nn.ReLU()
         self.final = torch.nn.Linear(d_embedding, 1)
+        self.lr = lr
         self.val_loss = MeanSquaredError()
         self.test_loss = MeanSquaredError()
-    
+        self.test_spearman = SpearmanCorrCoef()
+        
     def forward(self, x):
         x = self.relu(self.linear(x))
         x = self.final(x)
         return x
 
     def training_step(self, batch, batch_idx):
-        src, tgt, mask, _ = batch
-        pooled = self.pool(src, mask)
+        src, tgt, mask, dataset = batch
+        pooled = self.pool(src, mask, dataset)
         output = self(pooled)
 
-        return F.mse_loss(output, tgt.unsqueeze(-1))
+        output = output.flatten()
+        tgt = tgt.flatten()
+        
+        return F.mse_loss(output, tgt)
 
     def validation_step(self, batch, batch_idx):
-        src, tgt, mask, _ = batch
-        pooled = self.pool(src, mask)
+        src, tgt, mask, dataset = batch
+        pooled = self.pool(src, mask, dataset)
         output = self(pooled)
         
-        self.log("val_loss", self.val_loss(output, tgt), on_step=False, on_epoch=True)
-        return F.mse_loss(output, tgt.unsqueeze(-1))
+        output = output.flatten()
+        tgt = tgt.flatten()
+        
+        self.val_loss.update(output, tgt)
+        self.log("val_loss", self.val_loss, on_step=False, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
-        src, tgt, mask, _ = batch
-        pooled = self.pool(src, mask)
+        src, tgt, mask, dataset = batch
+        pooled = self.pool(src, mask, dataset)
         output = self(pooled)
         
-        self.log("test_loss", self.test_loss(output, tgt), on_step=False, on_epoch=True)
-        return F.mse_loss(output, tgt.unsqueeze(-1))
+        output = output.flatten()
+        tgt = tgt.flatten()
+        
+        self.test_spearman.update(output, tgt)
+        self.log("test_spearman", self.test_spearman, on_step=False, on_epoch=True)
+        self.test_loss.update(output, tgt)
+        self.log("test_loss", self.test_loss, on_step=False, on_epoch=True)
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam([
+            {'params': self.attention1d.parameters(), 'lr': 1e-3},
+            {'params': self.linear.parameters(), 'lr': 1e-3},
+            {'params': self.final.parameters(), 'lr': 1e-3}
+        ])
+        return optimizer
 
     def pool(self, x: torch.Tensor, mask: torch.Tensor, dataset: str) -> torch.Tensor:
-        x = self.model(x, repr_layers=[33])["representations"][33]
+        with torch.no_grad():
+            x = self.model(x, repr_layers=[33])["representations"][33]
         if self.pooling == "per_aa":
             x = self.attention1d(x, input_mask=mask) #TODO: mask will look different w non padding=1, will it still work the same
         elif self.pooling == "mean": #TODO: check that output matches mean pooled seqs generated from extract.py
@@ -85,12 +107,13 @@ class ESMTokenizer(Tokenizer):
     def __init__(self, model_name):
         if model_name not in ESM.name_to_model.keys():
             raise ValueError("Invalid model name provided")
-        _, self.alphabet = pretrained.load_model_and_alphabet(ESM.name_to_model[model_name])
-        self.pad_tok = self.alphabet.padding_idx
+        # https://github.com/facebookresearch/esm/blob/main/esm/data.py#L151, both esm1v/1b use this alphabet
+        self.alphabet = data.Alphabet.from_architecture('ESM-1b')
+        self._pad_tok = self.alphabet.padding_idx
 
     @property
     def pad_tok(self) -> int:
-        return self.pad_tok
+        return self._pad_tok
 
     def tokenize(self, seq: str) -> list[int]:
         tokenized = self.alphabet.encode(seq)
@@ -99,3 +122,5 @@ class ESMTokenizer(Tokenizer):
             tokenized.insert(0, self.alphabet.cls_idx)
         if self.alphabet.append_eos:
             tokenized.append(self.alphabet.eos_idx)
+        
+        return tokenized
